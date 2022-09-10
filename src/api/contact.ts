0fc5da32e2a7b47ba3277ce19d5b01e4dbc7a1f9
Mattia Cecchini <matcecco@gmail.com>
2022-09-10 22:44:34 +0200

    change api host

--- src/api/contact.ts
+++ src/api/contact.ts
@@ -1,12 +1,15 @@
 export const contactApi = {
   contactMe: async (data: Record<string, any>) => {
-    const result = await fetch('https://mcdev-bot.herokuapp.com/contact-me', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
+    const result = await fetch(
+      'https://mcdevhome.duckdns.org/mcdev-telegram-bot/contact-me',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }
+    );
     if (result.status !== 200) throw new Error('errore invio messaggio');
     return result.text();
   },
